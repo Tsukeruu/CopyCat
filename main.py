3e5e0b8b9dc456a72a7f9574f265f28930380d6a
Tsukeru <uzdbald@gmail.com>
2026-09-12 10:34:46 +0300

    Added database on .gitignore

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from pathlib import Path
 
 class main_UI(UVICORN): 
     def __init__(self) -> None:
-        load_dotenv(Path(__file__).parent / "SECRETS.env")
+        load_dotenv(Path(__file__).resolve().parent / "SECRETS.env")
         self.parser: ArgumentParser = ArgumentParser(description="A program that monitors tasks in the background and logs them using sqlmodel")
         self.parser.add_argument(
                 "-cs",
